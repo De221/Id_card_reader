dfc0dc1d3fc83d4960d46515abdcde5dc659a103
De221 <denits221@abv.bg>
2024-08-23 00:29:59 +0300

    updated-readme-installation

--- detectron_module.py
+++ detectron_module.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa425b309372cbad7d71c8c9efe68511f9d73619eb3cbbf28ca6d9b8ca819221
-size 3562
+oid sha256:7705da51cc1f07e75dbd92cfb70d8057e593d37edd0f3cef7c50dfed30c2cd22
+size 3549
