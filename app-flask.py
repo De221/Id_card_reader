2fed2f26642417d2ece1a7c73e2a4d644232a6fb
De221 <denits221@abv.bg>
2024-09-16 00:15:39 +0300

    contribute to the postprocessing functions

--- app-flask.py
+++ app-flask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c23d0f5b4e086b60f28332dc8256b5ede78c4ac1d4faa5355670e24206e6c66f
-size 14834
+oid sha256:54b97032796d36d40d0b1ec3c0072ae7525564f0deb4de80e59e89891487c1e4
+size 14857
